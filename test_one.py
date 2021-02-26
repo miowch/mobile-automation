6bb5e604fe68899ec034ca5e19e2e9a37563b483
Anastasia Kuzina <miowch@gmail.com>
2021-02-26 23:33:54 +0100

    Test 3 – attempt 1

--- test_one.py
+++ test_one.py
@@ -2,12 +2,16 @@
 
 class MainClass:
     __class_number = 20
+    __class_string = "Hello, world"
 
     def get_local_number(self):
         return 14
 
     def get_class_number(self):
         return MainClass.__class_number
+
+    def get_class_string(self):
+        return MainClass.__class_string
 
 
 class TestMainClass:
@@ -17,3 +21,8 @@ class TestMainClass:
     def test_get_class_number(self):
         assert MainClass.get_class_number(MainClass) > 45, "Class number is less than 45"
 
+    def test_get_class_string(self):
+        assert \
+            "hello" in MainClass.get_class_string(MainClass) or \
+            "Hello" in MainClass.get_class_string(MainClass), \
+            "Class string does not contain 'hello' or 'Hello'"
