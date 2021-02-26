d4fe7f8ec7b6e3156ecd80411c6c416ea7ad5e6e
Anastasia Kuzina <miowch@gmail.com>
2021-02-26 01:33:18 +0100

    Test 2 – attempt 1

--- test_one.py
+++ test_one.py
@@ -1,14 +1,19 @@
 # content of test_one.py
 
 class MainClass:
+    __class_number = 20
+
     def get_local_number(self):
         return 14
+
+    def get_class_number(self):
+        return MainClass.__class_number
 
 
 class TestMainClass:
     def test_get_local_number(self):
         assert MainClass.get_local_number(MainClass) == 14, "Local number does not equal 14"
 
+    def test_get_class_number(self):
+        assert MainClass.get_class_number(MainClass) > 45, "Class number is less than 45"
 
-if __name__ =="__main__":
-    TestMainClass.test_get_local_number(TestMainClass)
